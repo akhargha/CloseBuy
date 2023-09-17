b18803fae38004af8f14761e4b66f7ea4720cb00
Ki Hwang <kitae.hwang@trincoll.edu>
2023-09-16 20:42:11 -0400

    search + listview

--- src/lib/home_screen.dart
+++ src/lib/home_screen.dart
@@ -1,250 +1,86 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(new HomeScreen());
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListTileSelectExample(),
-      // body: SearchBar(),
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'ListView with Search'),
     );
   }
-
-
 }
-class ListTileSelectExample extends StatefulWidget {
-  const ListTileSelectExample({super.key});
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
   @override
-  ListTileSelectExampleState createState() => ListTileSelectExampleState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class ListTileSelectExampleState extends State<ListTileSelectExample> {
-  bool isSelectionMode = false;
-  final int listLength = 30;
-  late List<bool> _selected;
-  bool _selectAll = false;
-  bool _isGridMode = false;
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController editingController = TextEditingController();
+
+  final duplicateItems = List<String>.generate(10000, (i) => "Item $i");
+  var items = <String>[];
 
   @override
   void initState() {
+    items = duplicateItems;
     super.initState();
-    initializeSelection();
   }
 
-  void initializeSelection() {
-    _selected = List<bool>.generate(listLength, (_) => false);
-  }
-
-  @override
-  void dispose() {
-    _selected.clear();
-    super.dispose();
+  void filterSearchResults(String query) {
+    setState(() {
+      items = duplicateItems
+          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'ListTile selection',
-          ),
-          leading: isSelectionMode
-              ? IconButton(
-            icon: const Icon(Icons.close),
-            onPressed: () {
-              setState(() {
-                isSelectionMode = false;
-              });
-              initializeSelection();
-            },
-          )
-              : const SizedBox(),
-          actions: <Widget>[
-            if (_isGridMode)
-              IconButton(
-                icon: const Icon(Icons.grid_on),
-                onPressed: () {
-                  setState(() {
-                    _isGridMode = false;
-                  });
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  filterSearchResults(value);
                 },
-              )
-            else
-              IconButton(
-                icon: const Icon(Icons.list),
-                onPressed: () {
-                  setState(() {
-                    _isGridMode = true;
-                  });
+                controller: editingController,
+                decoration: InputDecoration(
+                    labelText: "Search",
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${items[index]}'),
+                  );
                 },
               ),
-            if (isSelectionMode)
-              TextButton(
-                  child: !_selectAll
-                      ? const Text(
-                    'select all',
-                    style: TextStyle(color: Colors.white),
-                  )
-                      : const Text(
-                    'unselect all',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    _selectAll = !_selectAll;
-                    setState(() {
-                      _selected =
-                      List<bool>.generate(listLength, (_) => _selectAll);
-                    });
-                  }),
+            ),
           ],
         ),
-        body: _isGridMode
-            ? GridBuilder(
-          isSelectionMode: isSelectionMode,
-          selectedList: _selected,
-          onSelectionChange: (bool x) {
-            setState(() {
-              isSelectionMode = x;
-            });
-          },
-        )
-            : ListBuilder(
-          isSelectionMode: isSelectionMode,
-          selectedList: _selected,
-          onSelectionChange: (bool x) {
-            setState(() {
-              isSelectionMode = x;
-            });
-          },
-        ));
-  }
-}
-
-class GridBuilder extends StatefulWidget {
-  const GridBuilder({
-    super.key,
-    required this.selectedList,
-    required this.isSelectionMode,
-    required this.onSelectionChange,
-  });
-
-  final bool isSelectionMode;
-  final Function(bool)? onSelectionChange;
-  final List<bool> selectedList;
-
-  @override
-  GridBuilderState createState() => GridBuilderState();
-}
-
-class GridBuilderState extends State<GridBuilder> {
-  void _toggle(int index) {
-    if (widget.isSelectionMode) {
-      setState(() {
-        widget.selectedList[index] = !widget.selectedList[index];
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-        itemCount: widget.selectedList.length,
-        gridDelegate:
-        const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (_, int index) {
-          return InkWell(
-            onTap: () => _toggle(index),
-            onLongPress: () {
-              if (!widget.isSelectionMode) {
-                setState(() {
-                  widget.selectedList[index] = true;
-                });
-                widget.onSelectionChange!(true);
-              }
-            },
-            child: GridTile(
-                child: Container(
-                  child: widget.isSelectionMode
-                      ? Checkbox(
-                      onChanged: (bool? x) => _toggle(index),
-                      value: widget.selectedList[index])
-                      : const Icon(Icons.image),
-                )),
-          );
-        });
-  }
-}
-
-class ListBuilder extends StatefulWidget {
-  const ListBuilder({
-    super.key,
-    required this.selectedList,
-    required this.isSelectionMode,
-    required this.onSelectionChange,
-  });
-
-  final bool isSelectionMode;
-  final List<bool> selectedList;
-  final Function(bool)? onSelectionChange;
-
-  @override
-  State<ListBuilder> createState() => _ListBuilderState();
-}
-
-class _ListBuilderState extends State<ListBuilder> {
-  void _toggle(int index) {
-    if (widget.isSelectionMode) {
-      setState(() {
-        widget.selectedList[index] = !widget.selectedList[index];
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: widget.selectedList.length,
-        itemBuilder: (_, int index) {
-          return ListTile(
-              onTap: () => _toggle(index),
-              onLongPress: () {
-                if (!widget.isSelectionMode) {
-                  setState(() {
-                    widget.selectedList[index] = true;
-                  });
-                  widget.onSelectionChange!(true);
-                }
-              },
-              trailing: widget.isSelectionMode
-                  ? Checkbox(
-                value: widget.selectedList[index],
-                onChanged: (bool? x) => _toggle(index),
-              )
-                  : const SizedBox.shrink(),
-              title: Text('item $index'));
-        });
-  }
-}
-class SearchBar extends StatefulWidget
-{
-  @override
-  _SearchBarState createState() => _SearchBarState();
-}
-
-
-class _SearchBarState extends State<SearchBar> {
-  final TextEditingController _searchController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      controller: _searchController,
-      decoration: InputDecoration(
-          hintText: 'Search...',
-          prefixIcon: Icon(Icons.search)
       ),
     );
   }
